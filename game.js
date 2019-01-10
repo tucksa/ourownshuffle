d5154960724831cd1355262f8dcfbe8a28c13f2a
Monty <tuckerjsarah@gmail.com>
2019-01-09 19:48:02 -0500

    shuffle in for loop

--- game.js
+++ game.js
@@ -1,25 +1,48 @@
- function randomInt(optArg= false) {
-        let x
-        if (optArg===false){
-          x= colors.length
-        }else{
-          x = optArg
-        }
-        return Math.floor(Math.random()*x);
-      } 
-        for (var i=0; i< 7; i++) {
-          Copy1= [...colors];
-          Copy2 = [...colors];
-          targetColor.push(Copy1.splice(randomInt(colors.length- i), 1));
-          targetColorOfText.push(Copy2.splice(randomInt(colors.length- i), 1));
-        };
-        console.log(targetColor);
-        console.log(targetColorOfText);.
+$(document).ready(function() {
 
-        for(var i= 0; i< 7; i++) {
+  // Array for word colors
+  var colorsOfText = [];
+
+  // Target word
+  var targetColor= [];
+
+  // Target word color
+  var targetColorOfText= [];
+
+  // User"s selection
+  var userAnswer;
+
+  // Array for words
+  var colors = [
+    "brown",
+    "blue",
+    "magenta",
+    "teal",
+    "coral",
+    "black"
+  ];
+ var Copy1= [...colors]
+  function randomInt() {
+        
+        return Math.floor(Math.random()*Copy1.length);
+      } 
+console.log(randomInt());
+console.log(colors[randomInt()]);
+
+
+      //targetColor.push(colors[randomInt()]);
+      for(var i= randomInt(); i<Copy1.length; i= randomInt()){
+        targetColor.push(Copy1[i]);
+        Copy1.splice(i,1);        
+      };
+        console.log(targetColor);
+
+        for(var i= 0; i< colors.length; i++) {
           div = document.createElement("li");
           $("#color-picker").append(div);
           $(div).attr("color" ,targetColor[i]).text(targetColor[i]);
           $(div).css("color", targetColorOfText[i]);
 
         }
+
+      });
